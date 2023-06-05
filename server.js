5505a70ce459648d2a342d7af258fee07b489cd3
mostafa <mostafa.elhabrok@gmail.com>
2023-06-05 18:39:50 +0300

    edit in server js

--- server.js
+++ server.js
@@ -3,31 +3,13 @@ const { ExpressPeerServer } = require("peer");
 
 const app = express();
 
-app.get("/", (req, res, next) => res.send("Hello world!"));
-
-// =======
 const port = process.env.PORT || 9000
 console.log("LISTEN TO: ",port);
 const server = app.listen(port);
 
 const peerServer = ExpressPeerServer(server, {
-	path: "/myapp",
+	path: "/",
+	allow_discovery:true
 });
 
-app.use("/peerjs", peerServer);
-
-// == OR ==
-
-// const http = require("http");
-
-// const server = http.createServer(app);
-// const peerServer = ExpressPeerServer(server, {
-// 	debug: true,
-// 	path: "/myapp",
-// });
-
-// app.use("/peerjs", peerServer);
-
-// server.listen(9000);
-
-// ========
+app.use("/", peerServer);
